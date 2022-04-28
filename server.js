f039ced57f3c2be91484dbb2d89926a611227083
Nghia Le <davidle314@hotmail.com>
2022-04-27 19:46:41 -0700

    adding server js with request to routes

--- server.js
+++ server.js
@@ -7,3 +7,15 @@ const express = require('express');
 
 const allNotes = require('./db/db.json');
 
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoute')(app);
+
+
+app.listen(PORT, () => {
+    console.log(`listening on localhost${PORT}`)
+});
